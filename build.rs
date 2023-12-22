8cc149a434d2fdc24d393c954c2fa15e8320627f
lixin <3050155685@qq.com>
2023-12-22 11:17:24 +0800

    grpc

--- build.rs
+++ build.rs
@@ -1,6 +1,17 @@
-fn main() {
-    prost_build::Config::new()
-        .out_dir("src/pb") //设置proto输出目录
-        .compile_protos(&["hello.proto"], &["."]) //我们要处理的proto文件
-        .unwrap();
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_file = "./protofile/store.proto";
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional") // for older systems
+        .build_client(true)
+        .build_server(true)
+        .file_descriptor_set_path(out_dir.join("store_descriptor.bin"))
+        .out_dir("./src")
+        .compile(&[proto_file], &["proto"])?;
+
+    Ok(())
 }
